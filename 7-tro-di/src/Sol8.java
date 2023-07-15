262ac32faad00aa00693ec5498c90fa33982459f
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-07-15 22:02:00 +0700

    commit

--- 7-tro-di/src/Sol8.java
+++ 7-tro-di/src/Sol8.java
@@ -1,3 +1,17 @@
 public class Sol8 {
-    
+    public static void main(String[] args){
+        System.out.println(myAtoi("42"));
+        System.out.println(myAtoi("     -42"));
+        System.out.println(myAtoi("4193 with words"));
+    }
+
+    public static int myAtoi(String s){
+        int num = 0;
+        Boolean isPositive = true;
+        Boolean isNumStarted = false;
+        for(int index = 0 ; index < s.length(); index++){
+
+        }
+        return isPositive ? num : num * -1;
+    }
 }
