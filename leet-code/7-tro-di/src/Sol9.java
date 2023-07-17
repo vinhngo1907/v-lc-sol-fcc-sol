41b5f50b562ee1c76288847b3d2e6c5fdb88cdfe
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-07-17 13:13:47 +0700

    commit

---
+++ leet-code/7-tro-di/src/Sol9.java
@@ -0,0 +1,16 @@
+public class Sol9 {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        int reverse = 0, copy = x;
+
+        while (copy > 0) {
+            int digit = copy % 10;
+            reverse = reverse * 10 + digit;
+            copy /= 10;
+        }
+
+        return reverse == x;
+    }
+}
