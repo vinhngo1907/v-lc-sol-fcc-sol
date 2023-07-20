69f219ea7b73e0af602bf0457aaeeb4f07564e0e
Koo Kuu <vinhtrungngo1907@gmail.com>
2023-07-20 15:31:20 +0700

    [commit] - commit

--- free-code-camp/JS_algothrim_vs_data_struct/Basic_Algorithm_Scipting/fibonacci.js
+++ free-code-camp/JS_algothrim_vs_data_struct/Basic_Algorithm_Scipting/fibonacci.js
@@ -7,21 +7,21 @@
  */
 
 // sol 1
-function fibonacci(n){
-    let f0 = 0, f1 = 1, fn = 1;
-    if(n < 0){
-      return -1;
-    }
-    
-    if(n == 0 || n ==1){
-      return n;
-    }
-    for(let i = 2;i < n; i++){
-     f0 = f1;
-      f1=fn;
-      fn = f0 + f1
-    }
-    return fn;
+function fibonacci(n) {
+  let f0 = 0, f1 = 1, fn = 1;
+  if (n < 0) {
+    return -1;
+  }
+
+  if (n == 0 || n == 1) {
+    return n;
+  }
+  for (let i = 2; i < n; i++) {
+    f0 = f1;
+    f1 = fn;
+    fn = f0 + f1
+  }
+  return fn;
 }
 
 // sol 2
@@ -29,14 +29,14 @@ function fibonacci(n){
 //     if(n < 0){
 //         return -1;
 //       }
-      
+
 //     if(n == 0 || n ==1){
 //         return n;
 //     }
-    
+
 //     return fibonacci(n -1 ) + fibonacci(n - 2);
 // }
 
-for(let i = 0; i < 10; i++){
-    console.log(fibonacci(i));
+for (let i = 0; i < 10; i++) {
+  console.log(fibonacci(i));
 }
---
+++ free-code-camp/JS_algothrim_vs_data_struct/Basic_Algorithm_Scipting/gcd.js
@@ -0,0 +1,20 @@
+
+/**
+ * Chuong trinh tim uoc chung lon nhat (USCLN)
+ * va boi so chung nho nhat (BSCNN) cua 2 so a và b
+ * 
+ * @author https://github.com/vinhngo1907
+ */
+
+function gcd(a, b) {
+    if (b == 0) return b;
+    return gcd(b, a % b);
+}
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+}
+
+const a = 4, b = 6;
+console.log(`USCLN ${a} & ${b}: ${gcd(a, b)}`)
+console.log(`BSCLN ${a} & ${b}: ${lcm(a, b)}`)
