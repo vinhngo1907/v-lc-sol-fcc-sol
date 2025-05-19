07d9b4a01f22e4dca12e4462aed4253e8a9a51f9
Koo Kuu <vinhtrungngo1907@gmail.com>
2025-05-19 14:59:01 +0700

    commit

---
+++ free-code-camp/JS_algothrim_vs_data_struct/OOP/addMethodAfterInher.js
@@ -0,0 +1,21 @@
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+
+Dog.prototype = Object.create(Animal.prototype)
+Dog.prototype.bark = function(){
+    console.log("Woof!")
+}
+
+
+Dog.prototype.constructor = Dog;
+// Only change code above this line
+
+let beagle = new Dog();
+
+console.log(beagle)
+console.log(beagle.eat())
+console.log(beagle.bark())
