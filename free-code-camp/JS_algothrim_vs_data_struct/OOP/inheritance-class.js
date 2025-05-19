0df958a0d8dfef86b2d73af733709f293fed3a81
Koo Kuu <vinhtrungngo1907@gmail.com>
2025-05-19 15:12:26 +0700

    OOP

---
+++ free-code-camp/JS_algothrim_vs_data_struct/OOP/inheritance-class.js
@@ -0,0 +1,40 @@
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    eat() {
+        console.log("eating...")
+    }
+}
+
+class Bird extends Animal {
+    constructor(name) {
+        super(name);
+    }
+
+    fly() {
+        console.log(`${this.name} is flying...`)
+    }
+}
+const barrot = new Bird("Con vec");
+barrot.fly()
+//
+function DongVat(name){
+    this.name = name;
+    this.eat = function(){
+        console.log(`${this.name} is eating...`)
+    }
+}
+DongVat.prototype.run = function(){
+    console.log(`${this.name} is running...`);
+}
+
+function Meo(){
+    DongVat.apply(this,arguments);
+}
+
+Meo.prototype = new DongVat();
+const tom = new Meo("Tom");
+tom.run();
+tom.eat()
---
+++ free-code-camp/JS_algothrim_vs_data_struct/OOP/override.js
@@ -0,0 +1,18 @@
+
+class CoffeeMachine {
+    makeCoffee() {
+        console.log("Making coffee...")
+    }
+}
+
+class SpecialCoffeeMachine extends CoffeeMachine {
+    makeCoffee(cb) {
+        console.log("Making coffee and do something...")
+        cb();
+    }
+}
+
+const coffeeMachine = new SpecialCoffeeMachine();
+coffeeMachine.makeCoffee(function () {
+    console.log('callback func is runed');
+});
